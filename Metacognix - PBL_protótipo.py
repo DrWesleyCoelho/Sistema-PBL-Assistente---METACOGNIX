ae80ce9b36e0d74064998ba12414ba56fefb0aa7
Wesley Coelho <178067263+DrWesleyCoelho@users.noreply.github.com>
2026-04-16 03:52:36 -0300

    Add files via upload

---
+++ Metacognix - PBL_protótipo.py
@@ -0,0 +1,59 @@
+  system_architecture:
+  core_engine: "Execução do método PBL"
+  knowledge_engine: "Integração com evidência científica"
+  user_engine: "Perfil cognitivo e adaptação"
+  document_engine: "Geração de relatórios (ABNT)"
+  evaluation_engine: "Score e comportamento PBL"
+  metacognix: "Tutor inteligente (IA)"
+  pbl_flow:
+  1: leitura_do_problema
+  2: termos_desconhecidos
+  3: hipoteses
+  4: estruturacao
+  5: objetivos_aprendizado
+  6: estudo_individual
+  7: rediscussao
+  motor de evidência cintífica
+  {
+  "fonte_prioritaria": [
+    "OMS",
+    "Ministério da Saúde",
+    "Diretrizes brasileiras",
+    "PubMed",
+    "NEJM",
+    "Lancet"
+  ],
+  "nivel_evidencia": "classificação IA, IB, II"
+}
+user_profile:
+  nivel:
+    - junior
+    - clinico
+    - autonomo
+  estilo_aprendizado:
+    - visual
+    - analitico
+    - hibrido
+  score_pbl: 0-100
+  PBL_Assistent:
+  subprojetos:
+    - subprojeto_1
+    - subprojeto_2
+    - subprojeto_3
+    - subprojeto_4
+    - subprojeto_5
+    - subprojeto_6
+    {
+  "bloqueio_objetivos": true,
+  "mensagem": "A geração automática de objetivos compromete o desenvolvimento do raciocínio clínico."
+}
+avaliacao:
+  questoes: 10
+  tipos:
+    - abertas
+    - fechadas
+  incluir:
+    - gabarito
+    - comentarios
+    - insights_clinicos
+    
